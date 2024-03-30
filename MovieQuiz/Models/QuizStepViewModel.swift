b8bb2f13bad53ac75ecafac9147cbeb76755437c
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-03-30 12:54:15 +0300

    Add QuestionFactory, AlertPresenter and StatisticService features

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let text: String
+    let questionNumber: String
+}
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+
+protocol StatisticServiceProtocol {
+    var totalAccuracy: Double { get }
+    var gamesCount: Int { get }
+    var bestGame: GameRecord { get }
+    
+    func store(correct count: Int, total amount: Int)
+}
+
+final class StatisticServiceImplementation: StatisticServiceProtocol {
+    private let userDefaults = UserDefaults.standard
+    var totalAccuracy: Double {
+        get {
+            let correct = userDefaults.double(forKey: Keys.correct.rawValue)
+            let total = userDefaults.double(forKey: Keys.total.rawValue)
+            return (correct / total) * 100
+        }
+    }
+    
+    var gamesCount: Int {
+        get {
+            return userDefaults.integer(forKey: Keys.gamesCount.rawValue)
+        }
+        
+        set {
+            userDefaults.setValue(newValue, forKey: Keys.gamesCount.rawValue)
+        }
+    }
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+                  let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                print("Невозможно сохранить результат")
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            return record
+        }
+        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        gamesCount += 1
+        
+        let correct = userDefaults.integer(forKey: Keys.correct.rawValue) + count
+        userDefaults.set(correct, forKey: Keys.correct.rawValue)
+        let total = userDefaults.integer(forKey: Keys.total.rawValue) + amount
+        userDefaults.set(total, forKey: Keys.total.rawValue)
+        
+        let newGame = GameRecord(correct: count, total: amount, date: Date())
+        if newGame.isBetterThan(bestGame) {
+            bestGame = newGame
+        }
+    }
+}
